a40182db0d28774157b815b8d6e3462bf179c78a
Akinpennu Taiwo <moriyebas@gmail.com>
2025-09-07 06:23:22 +0300

    mm

--- src/MachineLearning.jsx
+++ src/MachineLearning.jsx
@@ -2,6 +2,7 @@
 
 
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const ProjectCard = ({ title, description, images, githubLink, technologies, highlights, index }) => {
   const [isVisible, setIsVisible] = useState(false);
@@ -204,12 +205,12 @@ function MachineLearning() {
               Let's discuss how we can create impactful solutions together.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
-              <a
-                href="taakinpennu@gmail.com"
+              <Link
+                to="taakinpennu@gmail.com"
                 className="inline-flex items-center justify-center px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition-colors duration-200"
               >
                 Get in Touch
-              </a>
+              </Link>
               <a
                 href="https://linkedin.com/in/ptaiwo"
                 target="_blank"
